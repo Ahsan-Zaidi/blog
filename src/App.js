d3e388affb013834e866bc71fea2a3d2a05bea72
Zaidi <zaidiahsan.r@gmail.com>
2023-02-27 14:46:19 -0600

    routes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,38 @@ import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-      <Register />
-    </>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/register'>
+            <Register />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/write'>
+            <Write />
+          </Route>
+          <Route path='/settings'>
+            <Settings />
+          </Route>
+          <Route path='/post/:postId'>
+            <Single />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
