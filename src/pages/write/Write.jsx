e31f2f5cf43e3547e9c1299a06fa9f51b5600d3a
Zaidi <zaidiahsan.r@gmail.com>
2023-02-27 12:43:47 -0600

    write post page

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,9 +1,31 @@
 import './write.css';
+import postImg from '../../assets/img/Postimg.jpg';
 
 export default function Write() {
   return (
     <div className='write'>
-
+      <img 
+        className='write-img'
+        src={postImg}
+        alt='' 
+      />
+      <form className='write-form'>
+        <div className="write-form-group">
+          <label htmlFor='fileInput'>
+            <i className="write-icon fa-solid fa-plus"></i>
+          </label>
+          <input type='file' id='fileInput' style={{display:'none'}} />
+          <input type='text' placeholder='Title' className='write-input' autoFocus={true} />
+        </div>
+        <div className="write-form-group">
+          <textarea 
+            placeholder='Thoughts...' 
+            type='text' 
+            className='write-input write-text'
+            ></textarea>
+        </div>
+        <button className="write-submit">Publish</button>
+      </form>
     </div>
   )
 }
